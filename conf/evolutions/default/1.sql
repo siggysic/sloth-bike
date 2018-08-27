4eab70d15c6ffcdd7257c06f30645286146751c7
Pongsapak <pongsapak.k@scale360solutions.com>
2018-08-27 11:56:51 +0700

    fix evolution

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -85,6 +85,14 @@ CREATE TABLE histories (
       ON UPDATE CASCADE
     );
 
+CREATE TABLE Authentications (
+    Id VARCHAR(255) NOT NULL PRIMARY KEY,
+    Username VARCHAR(255),
+    Password VARCHAR(255),
+    Status VARCHAR(255),
+    Role VARCHAR(255)
+);
+
 
 # --- !Downs
 
@@ -94,3 +102,4 @@ DROP TABLE students
 DROP TABLE bikes
 DROP TABLE bike_status
 DROP TABLE stations
+DROP TABLE Authentications
