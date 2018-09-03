a7b8aeb3518dd0e282094313e09d7293416ef5dd
Siggy <siggysic@gmail.com>
2018-09-03 16:42:18 +0700

    Added new script sql

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,9 +1,9 @@
 # --- !Ups
-INSERT INTO bike_status(NAME) VALUES ('Available');
-INSERT INTO bike_status(NAME) VALUES ('Borrowed');
-INSERT INTO bike_status(NAME) VALUES ('OutOfOrder');
+INSERT INTO bike_status(ID, NAME) VALUES (1, 'Available');
+INSERT INTO bike_status(ID, NAME) VALUES (2, 'Borrowed');
+INSERT INTO bike_status(ID, NAME) VALUES (3, 'Out of order');
 
 # --- !Downs
 DELETE FROM bike_status WHERE NAME='Available';
 DELETE FROM bike_status WHERE NAME='Borrowed';
-DELETE FROM bike_status WHERE NAME='OutOfOrder';
+DELETE FROM bike_status WHERE NAME='Out of order';
